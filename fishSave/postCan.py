af730cf42864daf0f058cf2aeb75a38d0492172b
s-hirata0831 <e9248@g.maizuru-ct.ac.jp>
2024-07-20 09:07:09 +0900

    feat: Firebaseとゴミ箱側の接続が完了

--- fishSave/postCan.py
+++ fishSave/postCan.py
@@ -1,16 +1,22 @@
 import firebase_admin
 from firebase_admin import firestore
 from firebase_admin import credentials
-import time
-
 #------
 #Firebase初期設定
 #------
-cred = credentials.Certificate("token.json")
+cred = credentials.Certificate("fishSave/token.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 docCan = db.collection("stateTrashBox").document("can")
 docPet = db.collection("stateTrashBox").document("pet")
 
-docCan.update({"canNum": 24})
-docPet.update({"petNum": 20})
+#docCan.update({"canNum": 24})
+#docPet.update({"petNum": 20})
+
+can = docCan.get().to_dict()
+pet = docPet.get().to_dict()
+can_num = can.get('canNum', 0)
+pet_num = pet.get('petNum', 0)
+
+can_num += 1
+docCan.update({"canNum": can_num})
