d96d104570e58347a00e806883311864dd8ca03c
s-hirata0831 <e9248@g.maizuru-ct.ac.jp>
2024-07-22 16:14:25 +0900

    build: 本番環境ように調整

--- fishSave/postCan.py
+++ fishSave/postCan.py
@@ -6,11 +6,22 @@ import time
 #------
 #Firebase初期設定
 #------
-cred = credentials.Certificate("token.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
-docCan = db.collection("stateTrashBox").document("can")
-docPet = db.collection("stateTrashBox").document("pet")
+def postC():
+    cred = credentials.Certificate("fishSave/token.json")
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    docCan = db.collection("stateTrashBox").document("can")
+    can = docCan.get().to_dict()
+    can_num = can.get('canNum', 0)
+    can_num += 1
+    docCan.update({"canNum": can_num})    
 
-docCan.update({"canNum": 24})
-docPet.update({"petNum": 20})
+def postPet():
+    cred = credentials.Certificate("token.json")
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    docPet = db.collection("stateTrashBox").document("pet")
+    pet = docPet.get().to_dict()
+    pet_num=pet.get('petNum', 0)
+    pet_num += 1
+    docPet.update({"petNum": pet_num})
---
+++ fishSave/samplePost.py
@@ -0,0 +1,3 @@
+import postCan
+
+postCan.postC()
